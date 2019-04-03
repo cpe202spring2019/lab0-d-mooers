3e05cb3c9c2b3ed3b1f2a8fbe3f59d1a34131a8f
Dylan Mooers <dmooers@calpoly.edu>
2019-04-03 14:33:35 -0700

    Completed program, passes test

--- planets.py
+++ planets.py
@@ -1,6 +1,8 @@
 def weight_on_planets():
-   # write your code here
-   
+    earth_weight = int(input("What do you weigh on earth? "))
+    jupiter_weight = earth_weight * 2.34
+    mars_weight = earth_weight * .38
+    print("\nOn Mars you would weigh {0} pounds.\nOn Jupiter you would weigh {1} pounds.".format(mars_weight, jupiter_weight))   
    
    
 if __name__ == '__main__':
